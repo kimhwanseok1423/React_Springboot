969929bff60262be77287f4bcd56f8ba46c3a941
Hwan seok Kim <kcsss4499@gamil.com>
2024-08-31 23:39:29 +0900

    first init

---
+++ mall/src/pages/Mainpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BasicLayout from '../layouts/BasicLayout';
+
+const Mainpage = () => {
+  return (
+   <BasicLayout>
+    <div className={'text-3xl'}>Main Page</div>
+   </BasicLayout>
+  )
+}
+
+export default Mainpage;
